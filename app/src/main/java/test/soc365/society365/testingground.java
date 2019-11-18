6ddb6a75f27f04b42cb52bd32cb8155d4f727af8
shakir <chisti920@gmail.com>
2019-11-18 13:50:48 +0530

    second

---
+++ app/src/main/java/test/soc365/society365/testingground.java
@@ -0,0 +1,13 @@
+package test.soc365.society365;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testingground extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testingground);
+    }
+}
